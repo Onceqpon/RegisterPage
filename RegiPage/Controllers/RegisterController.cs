a2eb4a1947cf78f377b0c654a6a6504c938e6d18
dawid <dawid@ONCEQUPON>
2023-11-04 17:11:28 +0100

    Dodaj pliki projektów.

---
+++ RegiPage/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegiPage.Models;
+
+namespace RegiPage.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // Akcja obsługująca przesyłanie formularza
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Success", model);
+            }
+            else return View();
+
+        }
+        public IActionResult Success(User dane)
+        {
+
+            return View(dane);
+        }
+    }
+}
+
